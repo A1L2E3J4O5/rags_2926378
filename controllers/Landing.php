686b5ce1b10e7f2335cf74cb4706de9dbd2d84c5
Aprendiz <mailregister55@gmail.com>
2024-09-16 21:09:39 -0500

    actualizaciion vistas

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -5,12 +5,12 @@
         # Landing
         public function main(){
             // require_once "views/company/header.view.php";
-            require_once "views/company/index.view.php";
+            require_once "views/modules/reports/registros.view.php";
             // require_once "views/company/footer.view.php";
         }
         public function about(){
             // require_once "views/company/header.view.php";
-            require_once "views/company/about.view.php";
+            require_once "views/modules/reports/registros.view.php";
             // require_once "views/company/footer.view.php";
         }
     }
